733539a33e12cdc906610feddec9f5abac4ccec0
Ilham <ilhamshinohara@gmail.com>
2020-07-15 02:22:46 +0800

    update

--- client/src/app/first/first.component.ts
+++ client/src/app/first/first.component.ts
@@ -12,7 +12,7 @@ export class FirstComponent implements OnInit {
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('http://iqraa.herokuapp.com/api/books')
+    this.http.get('https://iqraa.herokuapp.com/api/books')
       .subscribe(data => {
         this.books = data;
       })
